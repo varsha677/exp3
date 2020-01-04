3dff12adb35d242991437fb0aeaf6eb377cbc5b3
varsha677 <59112412+varsha677@users.noreply.github.com>
2020-01-04 14:25:12 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.io.Scanner;
+class Add
+{
+public static void main(String[] args)
+{
+int a,b;
+int c=a+b;
+Scanner sc =new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.println("Sum is:"+c);
+}
+}
